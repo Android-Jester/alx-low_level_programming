407a71eb35f22c7e33e64b0c6c1f92451466f1c2
Duah Kwadwo Adjei <duah14@outlook.com>
2022-07-01 03:13:14 +0000

    low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * */
+
+
 int main() {
   printf("%s\n", "with proper grammer, but the outcome is a piece of art,");
   return 0;
--- 0x00-hello_world/6-sizes.c
+++ 0x00-hello_world/6-sizes.c
@@ -1,6 +1,11 @@
 #include <limits.h>
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * */
+
 int main(int argc, char *argv[]) {
   printf("Size of a char: %zu byte(s)\n", sizeof(char));
   printf("Size of a int: %zu byte(s)\n", sizeof(int));
